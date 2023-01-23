018dc59245d1e73757d266a6d9eb64c40545a9b5
clarence101701 <clarence.coronell@gmail.com>
2023-01-23 17:38:17 +0800

    add etching feature

--- js/script.js
+++ js/script.js
@@ -1,26 +1,56 @@
 let num = parseInt(prompt('Type a Number'));
 
-const area = document.querySelector('.sketch');
+const AREA = document.querySelector('.sketch');
 const column = [];
 const row = [];
 
-for(let i = 1; i <= num; i++){
+let active = false;
 
-    let currentRow = i-1;
+document.addEventListener('mousedown', () => active = true);
+document.addEventListener('mouseup', () => active = false);
+document.addEventListener('drag', () => active = false);
 
-    row[currentRow] = document.createElement('div');
-    row[currentRow].classList.add('test');
-    row[currentRow].classList.add('row');
-    area.appendChild(row[currentRow]);
+createSketch();
 
-    for(let j = 1; j <= num; j++){
-
-        let currentCol = j-1
-        
-        column[currentCol] = document.createElement('div');
-        column[currentCol].classList.add('test');
-        column[currentCol].classList.add('column');
-        row[currentRow].appendChild(column[currentCol]);
-    }
+function createSketch(){
     
+    
+    for(let i = 1; i <= num; i++){
+
+        let currentRow = i-1;
+    
+        row[currentRow] = document.createElement('div');
+        row[currentRow].classList.add('test');
+        row[currentRow].classList.add('row');
+        AREA.appendChild(row[currentRow]);
+    
+        for(let j = 1; j <= num; j++){
+    
+            let currentCol = j-1;
+            
+            column[currentCol] = document.createElement('div');
+            column[currentCol].classList.add('test');
+            column[currentCol].classList.add('column');
+            row[currentRow].appendChild(column[currentCol]);
+
+
+                column[currentCol].addEventListener('mouseenter', changeBg);
+                column[currentCol].addEventListener('mousedown', changeBg2);
+
+            
+        }
+        
+    }
 }
+
+function changeBg(){
+    if(active) {
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function changeBg2() {
+    this.style.backgroundColor = 'black';
+}
+
+
